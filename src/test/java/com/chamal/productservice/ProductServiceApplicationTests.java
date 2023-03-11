b7ed55bef88fd7687ff45b4a6442b156c11b39e9
Chamal-Peiris <chamal.peiris3g@gmail.com>
2023-03-11 10:38:55 +0530

    Initial commit

---
+++ src/test/java/com/chamal/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamal.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
