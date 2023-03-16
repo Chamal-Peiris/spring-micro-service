f6e0e23ee961bbcebcd6a3790f8fc601bc2ec9ed
Chamal-Peiris <chamal.peiris3g@gmail.com>
2023-03-16 22:33:58 +0530

    Add a fallback method for placeOrder API to handle fault tollerence

--- order-service/src/main/java/com/chamal/Order/service/controller/OrderController.java
+++ order-service/src/main/java/com/chamal/Order/service/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.chamal.Order.service.controller;
 
 import com.chamal.Order.service.dto.OrderRequestDto;
 import com.chamal.Order.service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,12 @@ public class OrderController {
     OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequestDto orderRequestDto) throws IllegalAccessException {
         orderService.PlaceOrder(orderRequestDto);
         return "Order Saved Successfully";
+    }
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
     }
 }
