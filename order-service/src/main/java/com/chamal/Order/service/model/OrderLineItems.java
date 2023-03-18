44299ee46feeea53800d21626162c5f031082098
Chamal-Peiris <chamal.peiris3g@gmail.com>
2023-03-18 21:02:09 +0530

    change some dependency versions in the project

--- order-service/src/main/java/com/chamal/Order/service/model/OrderLineItems.java
+++ order-service/src/main/java/com/chamal/Order/service/model/OrderLineItems.java
@@ -1,6 +1,6 @@
 package com.chamal.Order.service.model;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
